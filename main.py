d4720791a4ddccffafabd70fb8173b12b2a3ce29
head1ton <head1ton@gmail.com>
2023-07-28 02:23:44 +0900

    기본 베이스 작성

--- main.py
+++ main.py
@@ -3,14 +3,17 @@ import time
 import uvicorn
 from fastapi import FastAPI
 from loguru import logger
-from sqlalchemy import event, Engine
+from sqlalchemy import event
+from sqlalchemy.future import Engine
+from starlette.middleware.cors import CORSMiddleware
+
+from app.core.config import settings
 
 app = FastAPI(
-    version="0.0.1",
-    title="fastApiProject",
-    openapi_url=f"/api/v1/openapi.json",
+    version=settings.VERSION,
+    title=settings.PROJECT_NAME,
+    openapi_url=f"{settings.API_V1_STR}/openapi.json",
 )
-
 
 
 @event.listens_for(Engine, "before_cursor_execute")
@@ -24,6 +27,18 @@ def after_cursor_execute(conn, cursor, statement, parameters, context, executema
     total = time.time() - conn.info["query_start_time"].pop(-1)
     logger.info(f"QUERY PARAMS : ========================================> \n{parameters}")
     logger.info(f"TOTAL TIME: {total}")
+
+
+if settings.BACKEND_CORS_ORIGINS:
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=[str(origin) for origin in settings.BACKEND_CORS_ORIGINS],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_header=["*"],
+    )
+
+app.include_router(api_router, prefix=settings.API_V1_STR)
 
 
 if __name__ == "__main__":
